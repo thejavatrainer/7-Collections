43ee00457ca2b55d4f4339d3708da5c24d4a2012
AChiscar <Andreea.CHISCARI@frequentis.com>
2019-05-19 19:38:37 +0300

    hw

---
+++ module-six/src/main/java/com/homework/Homework.java
@@ -0,0 +1,28 @@
+/*
+ * COPYRIGHT: FREQUENTIS AG. All rights reserved.
+ *            Registered with Commercial Court Vienna,
+ *            reg.no. FN 72.115b.
+ */
+package com.homework;
+
+public class Homework {
+    /**
+     * Exercise 1.
+     * Write a method that takes a string and returns the number of unique characters in the string.
+     * It is expected that a string with the same character sequence may be passed several times to the method.
+     * Since the counting operation can be time consuming, the method should cache the results, so that when the method is given a string previously
+     * encountered,it will simply retrieve the stored result.
+     * Use collections and maps where appropriate.
+     */
+
+    /**
+     * Exercise 2.
+     * Write a program which creates a concordance of characters occurring in a string (i.e., which characters occur where in a string).
+     * Read the string from the command line.
+     *
+     * Running the program:
+     *
+     * >java Concordance Hello World
+     * {d=[9], o=[4, 6], r=[7], W=[5], H=[0], l=[2, 3, 8], e=[1]}
+     */
+}
